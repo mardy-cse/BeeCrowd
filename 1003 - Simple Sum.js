7df5a875b188a917cc085ec6136a6062e02a8076
John Liton Mardy <125699050+mardy-cse@users.noreply.github.com>
2023-02-18 02:59:23 +0600

    Delete 1003 - Simple Sum.js

--- 1003 - Simple Sum.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-var a=parseInt(lines.shift());
-var b=parseInt(lines.shift());
-var SOMA=a+b;
-console.log("SOMA = "+SOMA);
